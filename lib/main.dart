ec6143566576dc7dd61998ecd0be1484b776ec7c
Krzysztof Jachimiak <k.jachimiak.net@gmail.com>
2021-02-08 11:13:40 +0100

    dostosowanie do uwag

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:animation_routes_riverpod_divizion/screens/intro.dart';
-
 import 'package:animation_routes_riverpod_divizion/share/services.dart';
-
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,7 +10,7 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.white,
         //body: Second(),
-        body: PierwszaAnimacja(),
+        body: Intro(),
       ),
     ),
   );
